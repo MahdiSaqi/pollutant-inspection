da2a484226aaae269da89b408a4628a8db2697e3
mahdisaqi <mahdi.saqi@gmail.com>
2025-09-29 16:50:32 +0330

    version 1.4

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -36,6 +36,6 @@ class Constants {
 
   ///labels
   static const String clearForm = 'پاک کردن فرم';
-  static const String version = '1.3';
+  static const String version = '1.4'; ///7 mehre 1404
 
 }
